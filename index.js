18abc470a936ce70681d577a15debffd9b0639fb
Prakash <103762233+Prakash7708@users.noreply.github.com>
2025-05-08 19:40:27 +0530

    prakash

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get("/students",async function(req,res){
       }
 })
 
+
 app.post("/student", async function(req,res){
        try{
          // ** If we connect the data to Database we follow this 4 Steps
@@ -126,6 +127,73 @@ app.delete("/student/:id",async function(req,res){
   }
 }; 
 dbconnectionCheck();
+
+
+app.post("/register", async function(req,res){
+  try{
+    // ** If we connect the data to Database we follow this 4 Steps
+//Open the connection
+  const connection = await mongoClient.connect(URL)
+  //Select the DB
+  const db = connection.db("b35wd-tamil");
+  //Select the collection and do the operation
+  let email = await db.collection("users").findOne({email:req.body.email})
+  if(email){
+    await connection.close();
+    return res.json({
+      message:"Email already exists"
+    })
+  }
+  await db.collection("users").insertOne(req.body)
+  //Close the connection
+  await connection.close();
+  res.json({
+   message:"student added successfully",
+  })
+  }catch(error){
+   console.log(error)
+  }
+});
+
+app.post("/login", async function (req, res) {
+  try {
+    // Open the connection
+    const connection = await mongoClient.connect(URL);
+    // Select the DB
+    const db = connection.db("b35wd-tamil");
+
+    // Check if the email already exists
+    const user_doc = await db.collection("users").findOne({ email: req.body.email });
+
+    if (user_doc) {
+      // Email exists, redirect to dashboard or a welcome page
+      await connection.close();
+      console.log(user_doc);
+      console.log(req.body);
+      if(user_doc.password === req.body.password){
+        return res.json({
+          message: "Login successful",
+          user:user_doc,
+          redirectTo: "/dashboard", // Redirect to the dashboard or welcome page
+        });
+      }
+      else{
+        await connection.close();
+        return res.json({
+          message: "Incorrect password. Please try again."
+        });
+      }
+    }else{
+      await connection.close();
+      return res.json({
+        message: "Email not registered. Please register first."
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 // app.listen(3001);
 app.listen(process.env.PORT || 3001);
